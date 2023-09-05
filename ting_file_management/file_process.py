33b8f0a4e9702c84f4e6d78b25e0fdaf37e9be25
Eduardo-Malhao <eduardo.malhao@outlook.com>
2023-09-05 18:15:12 -0300

    File process Full

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,10 +1,35 @@
+from ting_file_management.file_management import txt_importer
+import sys
+
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+    lines = txt_importer(path_file)
+
+    if len(instance.data) > 0:
+        for elements in instance.data:
+            if elements["nome_do_arquivo"] == path_file:
+                return None
+
+    value = {
+        "nome_do_arquivo": path_file,
+        "qtd_linhas": len(lines),
+        "linhas_do_arquivo": lines,
+    }
+
+    instance.enqueue(value)
+    return print(value)
 
 
 def remove(instance):
-    """Aqui irá sua implementação"""
+    if len(instance.data) == 0:
+        return print("Não há elementos")
+
+    path_file = instance.data[0]
+    instance.dequeue()
+    return print(f"Arquivo {path_file['nome_do_arquivo']} removido com sucesso")
 
 
 def file_metadata(instance, position):
-    """Aqui irá sua implementação"""
+    try:
+        return print(instance.search(position))
+    except IndexError:
+        return sys.stderr.write("Posição inválida")
