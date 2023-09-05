3315b87c9b381a0adbc924572e7dd9d0dccfa9e9
Eduardo-Malhao <eduardo.malhao@outlook.com>
2023-09-05 17:12:10 -0300

    REQ 02

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,12 @@
+import sys
+
 def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+    doc = path_file.endswith(".txt")
+    try:
+        if not doc:
+             return sys.stderr.write("Formato inválido")
+        with open(path_file, "r") as file:
+            lines = file.read().split("\n")
+            return lines
+    except FileNotFoundError:
+        return sys.stderr.write(f"Arquivo {path_file} não encontrado\n")
