81e23c385f5a2e45d07c243dcae07968c8663efd
Eduardo-Malhao <eduardo.malhao@outlook.com>
2023-09-05 18:54:46 -0300

    Exist e Search WORD

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,6 +1,31 @@
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+    for item in instance.data:
+        found = search_by_word(word, instance["linhas_do_arquivo"])
+        if found == word:
+            response = {
+                "palavra": word,
+                "arquivo": item["nome_do_arquivo"],
+                "ocorências": len(search_by_word(word, instance["linhas_do_arquivo"])),
+            }
+    return response
 
 
 def search_by_word(word, instance):
-    """Aqui irá sua implementação"""
+    array = []
+    for item in range(0, len(instance)):
+        file = instance.search(item)
+        foundTimes = []
+        for element, line in enumerate(file["linhas_do_arquivo"]):
+            if word.lower() in line.lower():
+                foundTimes.append({"linha": element + 1})
+
+        if len(foundTimes) > 0:
+            array.append(
+                {
+                    "palavra": word,
+                    "arquivo": file["nome_do_arquivo"],
+                    "ocorências": len(foundTimes),
+                }
+            )
+    return array
+
