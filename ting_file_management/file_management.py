e8fdc90b1cccfd8c4a931b5dbb186f62d42398f6
Eduardo-Malhao <eduardo.malhao@outlook.com>
2023-09-05 21:31:28 -0300

    Descoment

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,10 +1,13 @@
+import sys
+
+
 def txt_importer(path_file):
     doc = path_file.endswith(".txt")
     try:
         if not doc:
-            raise ValueError("Formato inválido")
+            return sys.stderr.write("Formato inválido")
         with open(path_file, "r") as doc:
             lines = doc.read().split("\n")
             return lines
     except FileNotFoundError:
-        raise FileNotFoundError(f"Arquivo {path_file} não encontrado")
+        return sys.stderr.write(f"Arquivo {path_file} não encontrado\n")
--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,31 +1,31 @@
-# def exists_word(word, instance):
-#     for item in instance.data:
-#         found = search_by_word(word, instance["linhas_do_arquivo"])
-#         if found == word:
-#             response = {
-#                 "palavra": word,
-#                 "arquivo": item["nome_do_arquivo"],
-#                 "ocorências": len(search_by_word(
-#                     word, instance["linhas_do_arquivo"])),
-#             }
-#     return response
+def exists_word(word, instance):
+    for item in instance.data:
+        found = search_by_word(word, instance["linhas_do_arquivo"])
+        if found == word:
+            response = {
+                "palavra": word,
+                "arquivo": item["nome_do_arquivo"],
+                "ocorências": len(search_by_word(
+                    word, instance["linhas_do_arquivo"])),
+            }
+    return response
 
 
-# def search_by_word(word, instance):
-#     array = []
-#     for item in range(0, len(instance)):
-#         file = instance.search(item)
-#         foundTimes = []
-#         for element, line in enumerate(file["linhas_do_arquivo"]):
-#             if word.lower() in line.lower():
-#                 foundTimes.append({"linha": element + 1})
+def search_by_word(word, instance):
+    array = []
+    for item in range(0, len(instance)):
+        file = instance.search(item)
+        foundTimes = []
+        for element, line in enumerate(file["linhas_do_arquivo"]):
+            if word.lower() in line.lower():
+                foundTimes.append({"linha": element + 1})
 
-#         if len(foundTimes) > 0:
-#             array.append(
-#                 {
-#                     "palavra": word,
-#                     "arquivo": file["nome_do_arquivo"],
-#                     "ocorências": len(foundTimes),
-#                 }
-#             )
-#     return array
+        if len(foundTimes) > 0:
+            array.append(
+                {
+                    "palavra": word,
+                    "arquivo": file["nome_do_arquivo"],
+                    "ocorências": len(foundTimes),
+                }
+            )
+    return array
