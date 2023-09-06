8a4e8454996bd8fde878f06fdcb215ab0f580c0f
Eduardo-Malhao <eduardo.malhao@outlook.com>
2023-09-05 21:59:53 -0300

    Ajustando WordSearch

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,31 +1,34 @@
 def exists_word(word, instance):
-    for item in instance.queue:
-        found = search_by_word(word, instance["linhas_do_arquivo"])
-        if found == word:
-            response = {
+    array = []
+    for item in range(0, len(instance)):
+        files = instance.search(item)
+        found = []
+        for item, lines in enumerate(files["linhas_do_arquivo"]):
+            if word.lower() in lines.lower():
+                found.append({"linha": item + 1})
+        if found:
+            array.append({
                 "palavra": word,
-                "arquivo": item["nome_do_arquivo"],
-                "ocorências": len(search_by_word(
-                    word, instance["linhas_do_arquivo"])),
-            }
-    return response
+                "arquivo": files["nome_do_arquivo"],
+                "ocorências": found
+            })
+    return array
 
 
 def search_by_word(word, instance):
     array = []
     for item in range(0, len(instance)):
-        file = instance.search(item)
-        foundTimes = []
-        for element, line in enumerate(file["linhas_do_arquivo"]):
-            if word.lower() in line.lower():
-                foundTimes.append({"linha": element + 1})
-
-        if len(foundTimes) > 0:
+        files = instance.search(item)
+        found = []
+        for item, lines in enumerate(files["linhas_do_arquivo"]):
+            if word.lower() in lines.lower():
+                found.append({"linha": item + 1})
+        if found:
             array.append(
                 {
                     "palavra": word,
-                    "arquivo": file["nome_do_arquivo"],
-                    "ocorências": len(foundTimes),
+                    "arquivo": files["nome_do_arquivo"],
+                    "ocorências": found,
                 }
             )
     return array
